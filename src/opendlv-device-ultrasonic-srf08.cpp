dc298ff0d6800e37197c57bb4a8be0ce39e9d7c2
Björnborg Nguyen <bjornborg.nguyen@chalmers.se>
2018-04-25 20:53:25 +0200

    Test

--- src/opendlv-device-ultrasonic-srf08.cpp
+++ src/opendlv-device-ultrasonic-srf08.cpp
@@ -31,10 +31,15 @@
 int32_t main(int32_t argc, char **argv) {
   int32_t retCode{0};
   auto commandlineArguments = cluon::getCommandlineArguments(argc, argv);
-  if (0 == commandlineArguments.count("cid") || 0 == commandlineArguments.count("freq") || 0 == commandlineArguments.count("dev") || 0 == commandlineArguments.count("bus-address")) {
+  if (0 == commandlineArguments.count("cid") || 
+    0 == commandlineArguments.count("freq") || 
+    0 == commandlineArguments.count("dev") || 
+    0 == commandlineArguments.count("bus-address") ||
+    0 == commandlineArguments.count("range") ||  
+    0 == commandlineArguments.count("gain")) {
     std::cerr << argv[0] << " interfaces to an SRF02 ultrasonic distance sensor using i2c." << std::endl;
-    std::cerr << "Usage:   " << argv[0] << " --dev=<I2C device node> --bus-address=<Sensor address on the i2c bus, in decimal format> --freq=<Parse frequency> --cid=<OpenDaVINCI session> [--id=<ID if more than one sensor>] [--verbose]" << std::endl;
-    std::cerr << "Example: " << argv[0] << " --dev=/dev/i2c-0 --bus-address=112 --freq=10 --cid=111" << std::endl;
+    std::cerr << "Usage:   " << argv[0] << " --dev=<I2C device node> --bus-address=<Sensor address on the i2c bus, in decimal format> --freq=<Parse frequency> --cid=<OpenDaVINCI session> [--id=<ID if more than one sensor>]  --range=[decimal integer] --gain=[decimal integer][--verbose]" << std::endl;
+    std::cerr << "Example: " << argv[0] << " --dev=/dev/i2c-0 --bus-address=112 --freq=10 --cid=111 --range=100 --gain=1" << std::endl;
     retCode = 1;
   } else {
     uint32_t const ID{(commandlineArguments["id"].size() != 0) ? static_cast<uint32_t>(std::stoi(commandlineArguments["id"])) : 0};
@@ -53,6 +58,8 @@ int32_t main(int32_t argc, char **argv) {
     }
 
     uint8_t const address = std::stoi(commandlineArguments["bus-address"]);
+    uint8_t const range = std::stoi(commandlineArguments["range"]);
+    uint8_t const gain = std::stoi(commandlineArguments["gain"]);
 
     int8_t status = ioctl(deviceFile, I2C_SLAVE, address);
     if (status < 0) {
@@ -79,6 +86,23 @@ int32_t main(int32_t argc, char **argv) {
 
     cluon::OD4Session od4{CID};
 
+    uint8_t buf[2];
+    buf[0] = 0x02;
+    buf[1] = range;
+
+    uint8_t test = write(deviceFile, buf, 2);
+    if(test != 2) {
+      std::cout << "Error in changing the range." << std::endl;
+    }
+
+    buf[0] = 0x01;
+    buf[1] = gain;
+    test = write(deviceFile, buf, 2);
+    if(test != 2) {
+      std::cout << "Error in changing the gain." << std::endl;
+    }
+
+
     if (VERBOSE == 2) {
       initscr();
     }
@@ -95,47 +119,47 @@ int32_t main(int32_t argc, char **argv) {
         }
         std::this_thread::sleep_for(std::chrono::duration<double>(0.07));
         
-        uint8_t data{0x01};
+        uint8_t data{0x02};
       
-        uint8_t buffer[35];
+        uint8_t buffer[34];
 
         res = write(deviceFile, &data, 1);
-        res += read(deviceFile, &buffer, 35);
-        if (res != 36) {
+        res += read(deviceFile, &buffer, 34);
+        if (res != 35) {
           std::cerr << "Could not read data." << std::endl;
           return false;
         }
         // float lumen = static_cast<float>(data[0]) / 248.0f * 1000.0f;
-
         std::vector<float> val;
         for (uint8_t i = 0; i < 33; i+=2){
-          if (buffer[i+1] == 0 && buffer[i+2] == 0) {
+          if (buffer[i] == 0 && buffer[i+1] == 0) {
             break;
           }
-          uint32_t rangeCm = (buffer[i+1] << 8) + buffer[i+2];
-          val.push_back(static_cast<float>(rangeCm / 100.0f)); 
+          uint16_t rangeCm = (buffer[i] << 8) | buffer[i+1];
+          val.push_back(static_cast<float>(rangeCm) / 100.0f); 
         }
 
+        if (!val.empty()) {
+          opendlv::proxy::DistanceReading distanceReading;
+          distanceReading.distance(val.at(0));
 
-        // opendlv::proxy::DistanceReading distanceReading;
-        // distanceReading.distance(distance1);
-
-        // cluon::data::TimeStamp sampleTime;
-        // od4.send(distanceReading, sampleTime, ID);
-        if (VERBOSE == 1) {
-          // std::cout << "SRF08 distance reading is " << distanceReading.distance() << " and " << distance2 << "m. Brightness is " << lumen << " lumen." << std::endl;
-          // std::cout << "Measured:"
+          cluon::data::TimeStamp sampleTime = cluon::time::now();
+          od4.send(distanceReading, sampleTime, ID);
+          if (VERBOSE == 1) {
+            std::cout << "SRF08 distance reading is " << distanceReading.distance() << "m." << std::endl;
+          }
         }
 
-      if (VERBOSE == 2) {
-        std::stringstream ss;
-        for (uint8_t i = 0; i < val.size(); i++) {
-          ss << i << ": " << val.at(i) << std::endl;
+        if (VERBOSE == 2) {
+          std::stringstream ss;
+          clear();
+          mvprintw(1,1,("size of data: "+std::to_string( val.size() )).c_str());
+          for (uint8_t i = 0; i < val.size(); i++) {
+            std::string str = std::to_string(i) + ": "+ std::to_string(val.at(i));
+            mvprintw(i+2,1,str.c_str());
+          }
+          refresh();      /* Print it on to the real screen */
         }
-        mvprintw(1,1,ss.str().c_str()); 
-        refresh();      /* Print it on to the real screen */
-      }
-        
         return true;
       }};
 
